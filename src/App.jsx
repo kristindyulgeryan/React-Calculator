e850b33f2c0e9309107def134e8f0abb919c77cf
Kristin <kristindyulgeryan@abv.bg>
2025-02-25 16:25:07 +0200

    Set up styling for calculator project

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="calculator-grid">
      <div className="output">
-      <div className="previous-operand"></div>
-      <div className="current-operand"></div>
+      <div className="previous-operand">12,154*</div>
+      <div className="current-operand">123,123</div>
      </div>
      <button className="span-two">AC</button>
      <button >DEL</button>
